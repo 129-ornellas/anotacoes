3723e3da960288298b7faa93bc1efdf69864068d
ornellas <129.ornellas@gmail.com>
2024-01-20 01:21:50 -0300

    botão separado em buttongroup

--- frontend/pages/app/criar.vue
+++ frontend/pages/app/criar.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="create-page my-8 mx-auto">
+  <div class="create-page my-8 px-4 mx-auto">
     <v-textarea variant="outlined" label="Texto"></v-textarea>
     <v-text-field variant="outlined" prefix="R$" label="Potencial do Negócio"></v-text-field>
     <v-select variant="outlined" chips label="Categorização" multiple></v-select>
     <v-text-field variant="outlined" label="Lembrete" type="date"></v-text-field>
-    <VBtn variant="flat" size="large" color="blue">Salvar</VBtn>
-    <VBtn variant="flat" color="red" icon="delete"></VBtn>
+    <v-btn-group variant="outlined" divided border style="width: 100%">
+      <VBtn variant="flat" size="large" style="width: 90%">Salvar</VBtn>
+      <VBtn variant="flat" icon="delete" style="width: 10%"></VBtn>
+    </v-btn-group>
   </div>
 </template>
+
 <style>
   .create-page {
     max-width: 800px;
