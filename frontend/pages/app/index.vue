c31e39c0b72e630cd2cdbe1e9f67130b330ba591
ornellas <129.ornellas@gmail.com>
2024-01-20 00:31:33 -0300

    adiciona home

--- frontend/pages/app/index.vue
+++ frontend/pages/app/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <VBtn variant="flat" color="primary" prepend-icon="add"> Criar Anotação</VBtn>
+  <div class="app-page">
+    <VIcon class="ap-icon mb-2" size="48px">attach_file</VIcon>
+    <h1 class="mb-4">Anotações</h1>
+    <VBtn variant="flat" color="primary" prepend-icon="add"> Criar Anotação</VBtn>
+  </div>
 </template>
+
+<style>
+  .app-page {
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .ap-icon {
+    transform: rotate(225deg);
+  }
+</style>
