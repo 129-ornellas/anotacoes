4386326f3af0b3d58058deb69f890ec7685120a5
ornellas <129.ornellas@gmail.com>
2024-01-20 01:38:55 -0300

    cria listar

--- frontend/pages/app/criar.vue
+++ frontend/pages/app/criar.vue
@@ -3,7 +3,7 @@
     <v-textarea variant="outlined" label="Texto"></v-textarea>
     <v-text-field variant="outlined" prefix="R$" label="Potencial do Negócio"></v-text-field>
     <v-select variant="outlined" chips label="Categorização" multiple></v-select>
-    <v-text-field variant="outlined" label="Lembrete" type="date"></v-text-field>
+    <v-text-field variant="outlined" label="Lembrete" type="datetime-local"></v-text-field>
     <v-btn-group variant="outlined" divided border style="width: 100%">
       <VBtn variant="flat" size="large" style="width: 90%">Salvar</VBtn>
       <VBtn variant="flat" icon="delete" style="width: 10%"></VBtn>
---
+++ frontend/pages/app/listar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="list-page my-8 px-4 mx-auto">
+    <v-card variant="flat" border prepend-icon="home">
+      <template #prepend>
+        <VIcon>attach_file</VIcon>
+      </template>
+      <template #title> This is a title </template>
+      <template #subtitle> 27/05/2024 - 23:12</template>
+      <template #append>
+        <VIcon>delete</VIcon>
+      </template>
+    </v-card>
+  </div>
+</template>
+
+<style>
+  .list-page {
+    max-width: 800px;
+  }
+</style>
