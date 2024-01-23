91dab3a72ed052affb92a0eb6db8aa20a373b0e8
ornellas <129.ornellas@gmail.com>
2024-01-22 23:24:34 -0300

    cria api

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,12 +1,37 @@
+import { PrismaClient } from "@prisma/client";
 import express from 'express';
-
+const prisma = new PrismaClient()
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/annotations', async (req, res) => {
+    const annotation =  await prisma.annotation.create({
+            data: {
+                title: 'aaa',
+                category: 'letra',
+                remind_in: new Date(),
+                created_at: new Date()
+            }
+        })
+    console.log(annotation)
+})
+
+app.get('/annotations', async (req, res) => {
+    const annotation =  await prisma.annotation.findMany()
+    console.log(annotation)
+})
+
+app.patch('/annotations', async (req, res) => {
+    const annotation =  await prisma.annotation.update({
+        where:{
+            id: 2
+        },
+        data:{
+            deleted_at: new Date()
+        }
+    })
+    console.log(annotation)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`)})
