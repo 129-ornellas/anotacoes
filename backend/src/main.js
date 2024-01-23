fe422593d8ac01d6520eea755203a34751c1bb72
ornellas <129.ornellas@gmail.com>
2024-01-23 20:40:40 -0300

    formata codigo

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,38 +1,40 @@
-import { PrismaClient } from "@prisma/client";
-import express from 'express';
+import { PrismaClient } from "@prisma/client"
+import express from "express"
+
 const prisma = new PrismaClient()
 const app = express()
 const port = 8000
 
-app.post('/annotations', async (req, res) => {
-    const annotation =  await prisma.annotation.create({
-            data: {
-                title: 'aaa',
-                category: 'letra',
-                remind_in: new Date(),
-                created_at: new Date()
-            }
-        })
-    console.log(annotation)
+app.post("/annotations", async (req, res) => {
+  const annotation = await prisma.annotation.create({
+    data: {
+      title: "aaa",
+      category: "letra",
+      remind_in: new Date(),
+      created_at: new Date(),
+    },
+  })
+  console.log(annotation)
 })
 
-app.get('/annotations', async (req, res) => {
-    const annotation =  await prisma.annotation.findMany()
-    console.log(annotation)
+app.get("/annotations", async (req, res) => {
+  const annotation = await prisma.annotation.findMany()
+  console.log(annotation)
 })
 
 // safe delete
-app.patch('/annotations', async (req, res) => {
-    const annotation =  await prisma.annotation.update({
-        where:{
-            id: 2
-        },
-        data:{
-            deleted_at: new Date()
-        }
-    })
-    console.log(annotation)
+app.patch("/annotations", async (req, res) => {
+  const annotation = await prisma.annotation.update({
+    where: {
+      id: 2,
+    },
+    data: {
+      deleted_at: new Date(),
+    },
+  })
+  console.log(annotation)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)})
+  console.log(`Example app listening on port ${port}`)
+})
