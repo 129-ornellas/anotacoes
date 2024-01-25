bf6d0245da8db53134042233d79145a7be621596
ornellas <129.ornellas@gmail.com>
2024-01-25 01:33:36 -0300

    cria modal de deletar anotacao

---
+++ frontend/components/Deleta.vue
@@ -0,0 +1,52 @@
+import type { Axios } from 'axios';
+<template>
+  <v-dialog width="500">
+    <template #activator="{ props }">
+      <VBtn variant="flat" icon="delete" v-bind="props"></VBtn
+    ></template>
+
+    <template #default="{ isActive }">
+      <v-card title="Excluir">
+        <h1></h1>
+        <v-card-text> {{ id }} </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Cancelar" @click="isActive.value = false"></v-btn>
+          <v-btn
+            color="red"
+            text="Excluir"
+            @click="(isActive.value = false), deletaAnotacao()"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+  import axios from "axios"
+  export default {
+    props: {
+      id: { type: Number, required: true },
+    },
+    emits: ["id-removido"],
+    methods: {
+      deletaAnotacao() {
+        const url = `http://localhost:8000/annotations/${this.id}`
+        console.log(url)
+        axios
+          .delete(url)
+          .then((response) => {
+            if (response.status === 200) {
+              this.$emit("id-removido", this.id)
+            }
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      },
+    },
+  }
+  //   console.log(this.key)
+</script>
