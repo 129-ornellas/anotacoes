1e1a004393a73e72233e11b049916e5582ad23eb
ornellas <129.ornellas@gmail.com>
2024-01-25 18:17:00 -0300

    inclui anotacoes dentro da sidebar

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,46 @@
 <template>
   <VApp>
     <VAppBar border flat> <VAppBarTitle>Anotações</VAppBarTitle></VAppBar>
-    <VNavigationDrawer permanent> </VNavigationDrawer>
+
+    <VNavigationDrawer>
+      <v-card v-for="item in items" :key="item.id" variant="flat" border prepend-icon="home">
+        <template #prepend>
+          <VIcon>attach_file</VIcon>
+        </template>
+        <template #title> {{ item.title }} </template>
+        <template #subtitle>{{ item.remind_in }}</template>
+        <template #append>
+          <VIcon><Deleta :id="item.id" @id-removido="removerItem"></Deleta></VIcon>
+        </template>
+      </v-card>
+    </VNavigationDrawer>
     <VMain>
       <slot />
     </VMain>
   </VApp>
 </template>
+
+<script>
+  import axios from "axios"
+  export default {
+    data() {
+      return {
+        items: null,
+      }
+    },
+    async mounted() {
+      try {
+        const response = await axios.get("http://localhost:8000/annotations/")
+        this.items = response.data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    methods: {
+      removerItem(id) {
+        const newItems = this.items.filter((item) => item.id !== id)
+        this.items = newItems
+      },
+    },
+  }
+</script>
